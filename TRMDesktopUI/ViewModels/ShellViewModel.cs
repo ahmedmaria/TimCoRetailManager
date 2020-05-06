7a8b2bc55e923252140f1296f2792763489f41a7
ahmedmaria <ritu329@gmail.com>
2020-05-05 21:47:17 -0400

    Wire up login form to API
    Connects the Login form button to the authentication API endpoint (/token). Gets back the bearer token or an exeption if it fails.

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace TRMDesktopUI.ViewModels
 {
-  public  class ShellViewModel
+  public  class ShellViewModel : Conductor<object> //https://caliburnmicro.com/documentation/composition
     {
+        private LoginViewModel _loginVM;
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItem(_loginVM);
+
+        }
     }
 }
